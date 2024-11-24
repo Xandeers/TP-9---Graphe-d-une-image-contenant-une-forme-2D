e7bdd517771f5f59a2ab5b7b4239e569f47dacab
p2006707 <nedim.trad@etu.univ-lyon1.fr>
2024-11-24 14:48:54 +0100

    ajout image.h

---
+++ Image.h
@@ -0,0 +1,22 @@
+#ifndef IMAGE_
+#define IMAGE_
+#include<iostream>
+
+class Image
+{
+private:
+
+    int l,c ; //longueur, colone(largeur) d'une image 
+    int *tab; //tableau 1d de taille l*c qui contiendra l'image
+
+
+
+Image();//constructeur par defaut 
+
+~Image();//destructeur
+
+
+
+};
+#endif
+
